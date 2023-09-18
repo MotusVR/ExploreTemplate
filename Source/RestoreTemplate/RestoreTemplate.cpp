a3ff3d7c369cc2918a087e3b85a15a0f465e81cc
Tom Baker <thomasmebaker@gmail.com>
2023-09-18 12:24:52 +0100

    adding explore tempalte

---
+++ Source/RestoreTemplate/RestoreTemplate.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RestoreTemplate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RestoreTemplate, "RestoreTemplate" );
